d6064f6e45b09dd7e3bd099e20fa431e6d76e319
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-11-06 16:41:41 +0700

    PBOIF2-10119082-Latihan40-Warna Kepribadian

---
+++ src/pboif2/pkg10119082/latihan40/warna/kepribadian/PBOIF210119082Latihan40WarnaKepribadian.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan40.warna.kepribadian;
+import java.util.Scanner;
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi: Warna Kepribadian
+ */
+public class PBOIF210119082Latihan40WarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+
+        String warna,nama;
+
+        System.out.println(Warna.RED+"YUK"+Warna.GREEN+" CEK"+Warna.YELLOW+" KEPRIBADIANMU"+Warna.CYAN+" DARI"+Warna.PURPLE+" WARNA"+Warna.BLUE+" FAVORITMU");
+        System.out.println(Warna.RED+"MERAH");
+        System.out.println(Warna.GREEN+"HIJAU");
+        System.out.println(Warna.YELLOW+"KUNING");
+        System.out.println(Warna.BLUE+"BIRU");
+        System.out.println(Warna.PURPLE+"UNGU");
+        System.out.println(Warna.BLACK+"");
+        System.out.println("*gunakan huruf kapital");
+        System.out.print("PILIH WARNA FAVORITMU : ");
+        warna = input.next();
+        System.out.print("NAMA KAMU : ");
+        nama = input.next();
+        Warna.tesKepribadianWarna(warna, nama);
+        System.out.println("Developed by : Zainul Rifqi Muwaffaq");
+    }
+    
+}
